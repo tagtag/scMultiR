512f556b3d50896c9b12661466c9da8470b8105f
Y-h. Taguchi <tag@granular.com>
2021-09-12 22:47:21 +0900

    Add files via upload

---
+++ dataset1/GCH/batch_1pp.R
@@ -0,0 +1,38 @@
+require(rtracklayer)
+require(Matrix)
+bw <- import("GSM4678346_DNA_GO1_sc1.GCH.bw")
+chr<-seqnames(bw)@values
+load("ID_NDR")
+files <- list.files("./",pattern="GSM")
+TABLE<-table(unlist(lapply(strsplit(files,"_"),"[",3)))
+CL <- names(TABLE)
+j<-1
+cat("j=",j, "\n")
+load(paste("ID_all",CL[j],sep="_"))
+load(paste("sm_all",CL[j],sep="_"))
+SPLT_all <- NULL
+for (i in c(1:length(chr)))
+{
+    cat("i=",i,"\n")
+    index <- grep(paste(chr[i],"_",sep=""),ID_all)
+    num <- gsub(paste(chr[i],"_",sep=""),"",ID_all[index])
+    breaks <- ID_NDR[ID_NDR[,1]==chr[i],2]
+    breaks <- c(breaks,breaks[length(breaks)]+200)
+    num <- as.numeric(num)
+    CUT <-cut(num,breaks)
+    SPLT_temp<-NULL
+    for (k in c(1:dim(sm_all)[2]))
+    {
+    cat(k, " ")
+    SPLT <- split(sm_all[index,k],CUT)
+    SPLT_sum <- lapply(SPLT,sum)
+    SPLT_sum <- unlist(SPLT_sum)
+    ix <- (1:length(SPLT_sum))[SPLT_sum!=0]
+    value <- SPLT_sum[ix]
+    iy <- rep(1,length(ix))
+    sm <- sparseMatrix(ix,iy,x=value,dims=c(length(breaks)-1,1))
+    SPLT_temp <- cbind(SPLT_temp,sm)
+    }
+    SPLT_all <- rbind(SPLT_all,SPLT_temp)
+}
+save(file=paste("SPLT_all",CL[j],sep="_"),SPLT_all)
---
+++ dataset1/GCH/batch_4p.R
@@ -0,0 +1,22 @@
+require(rtracklayer)
+require(Matrix)
+files <- list.files("./",pattern="GSM")
+TABLE<-table(unlist(lapply(strsplit(files,"_"),"[",3)))
+CL <- names(TABLE)
+j=4
+cat("\nj=",j,"\n")
+files1 <- files[grep(CL[j],files)]
+load(paste("ID_all_",CL[j],sep=""))
+sm_all <- NULL
+for (i in c(1:length(files1)))
+{
+cat(i," ")
+bw <- import(files1[i])
+ID <- paste(seqnames(bw),start(bw),sep="_")
+ix <- match(ID,ID_all)
+iy <- rep(1,length(ix))
+value <- score(bw)
+sm <- sparseMatrix(ix,iy,x=value,dims=c(length(ID_all),1))
+sm_all <- cbind(sm_all,sm)
+}
+save(file=paste("sm_all_",CL[j],sep=""),sm_all)
